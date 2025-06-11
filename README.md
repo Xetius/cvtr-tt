25cd0023d870a99f04c854f11047b68e3c31e7ff
Chris Hudson <chris@Annelis-MacBook-Air.local>
2025-06-11 12:33:47 +0100

    Create initial VPC

---
+++ README.md
@@ -0,0 +1,9 @@
+# Convertr Technical Test
+# Chris Hudson
+
+ChangeLog
+* Configure AWS provider.  Using latest release version (5.99.1)
+* Configured for my AWS account in eu-west-2
+* Create VPC with no public IP address (CIDR 10.0.0.0/24)
+* Associated 2 private subnets (CIDR 10.0.1.0/24 and 10.0.2.0/24)
+* Create routing table and association
---
+++ main.tf
@@ -0,0 +1,37 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "private-vpc"
+  }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "private" {
+  count                   = length(var.private_subnet_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.private_subnet_cidrs[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "private-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
