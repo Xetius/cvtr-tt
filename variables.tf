d00558c98070aab07da6cc3e361b6db2954175c3
Chris Hudson <chris@Annelis-MacBook-Air.local>
2025-06-13 06:54:43 +0100

    Added types to variables

--- variables.tf
+++ variables.tf
@@ -1,15 +1,19 @@
 variable "region" {
+  type    = string
   default = "eu-west-2"
 }
 
 variable "vpc_cidr" {
+  type    = string
   default = "10.0.0.0/16"
 }
 
 variable "private_subnet_cidrs" {
+  type    = list(string)
   default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "bucket_name" {
+  type    = string
   default = "xetius-convertr-image-bucket"
 }
