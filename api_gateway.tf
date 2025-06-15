d7bcb7438316c66c7d6e795f87b26f2711bf61d2
Chris Hudson <chris@Annelis-MacBook-Air.local>
2025-06-15 17:44:14 +0100

    Removed authorizer

--- api_gateway.tf
+++ api_gateway.tf
@@ -47,8 +47,7 @@ resource "aws_api_gateway_method" "proxy_method" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.proxy.id
   http_method   = "ANY"
-  authorization = "CUSTOM"
-  authorizer_id = aws_api_gateway_authorizer.allow_all_authorizer.id
+  authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "proxy_integration" {
